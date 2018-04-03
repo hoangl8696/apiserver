c58c22c773b762a524f2e4ea299d3856a16404c2
Hong Hoang <ly.honghoang@cgi.com>
2018-04-03 16:31:28 +0300

    increase security by using env variable for google id and secret

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,6 @@ module.exports = {
     FILE_SIZE_IN_MB: 1024, //1GB
     LOG_DIR_DEV: "./logs-dev" ,
     LOG_DIR_PROD: "./logs-prod", //NOT IN USE, FAIL FEATURE, NEED FURTHER TESTING
-    GOOGLE_PLUS_API_CLIENT_ID: "764774255701-qhjngivkg8q6jdfiak1ibhgblena9p9e.apps.googleusercontent.com",
-    GOOGLE_PLUS_API_CLIENT_SECRET: "UK0LW03a4W6iJn8HJ-CBXoHl"
+    GOOGLE_PLUS_API_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "764774255701-qhjngivkg8q6jdfiak1ibhgblena9p9e.apps.googleusercontent.com",
+    GOOGLE_PLUS_API_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "UK0LW03a4W6iJn8HJ-CBXoHl"
 };
