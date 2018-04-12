a86a77953f7938bb4758c441ee9401107b73e4c5
Hong Hoang <ly.honghoang@cgi.com>
2018-04-12 15:48:15 +0300

    fix small bug

--- src/controller/image.js
+++ src/controller/image.js
@@ -82,11 +82,6 @@ module.exports.getAllImages = async (req,res) => {
         if (!result || result.length === 0) {
             return res.status(400).json({err: "no image was found"});
         }
-        const preparedResponse = result.map(file => {
-            file.id = file[_id];
-            file._id = "";
-            return file;
-        }) 
        return res.status(200).json(result);
     } catch (err) {
         return res.status(500).json({err});
